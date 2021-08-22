431fe56d69ec9ecf308622ad8202d2050f76128c
donvdkrogt <don.k@me.com>
2021-08-22 21:00:18 +0200

    move ssh-agent to .bashrc

--- .config/fish/functions/ssh_agent.fish
+++
@@ -1,9 +0,0 @@
-function ssh_agent
-  if ! pgrep -u $USER ssh-agent > /dev/null
-    ssh-agent -t 8h > $XDG_RUNTIME_DIR/ssh-agent.env
-  end
-  if ! test $SSH_AUTH_SOCK
-    bass source $XDG_RUNTIME_DIR/ssh-agent.env > /dev/null
-  end
-end
-
